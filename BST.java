38a101036fd1340214f7fe4335404d93a73eaea6
dheemanthp <dvp499@gmail.com>
2019-10-14 17:18:37 -0700

    Update BST.java

--- BST.java
+++ BST.java
@@ -303,7 +303,10 @@ int diameter(Node node) {
 
 // Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 //Note: A leaf is a node with no children.
-  
+ //Input Tree [5,4,8,11,null,13,4,7,2,null,null,5,1]
+ // Sum :22
+ //Output : [[5,4,11,2],[5,8,4,5]]
+
     public List<List<Integer>> pathSum(Node root, int sum) {
         List<List<Integer>> output = new ArrayList<List<Integer>>();
         List<Integer> current =  new ArrayList<Integer>();

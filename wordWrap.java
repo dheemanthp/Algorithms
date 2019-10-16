b20e6628d83887b66f1cb4668c601bb97b6a2c86
dheemanthp <dvp499@gmail.com>
2019-10-15 17:44:02 -0700

    Update wordWrap.java

--- wordWrap.java
+++ wordWrap.java
@@ -1,3 +1,33 @@
+/*
+https://webcache.googleusercontent.com/search?q=cache:iTgLVqb7nXsJ:https://leetcode.com/discuss/interview-question/398043/microsoft-oa-2019-word-wrap+&cd=1&hl=en&ct=clnk&gl=us
+Given a string s and an integer k. Break the string in such a way that:
+
+    The resulting string should not contain parts of a word.
+    The resulting string should not contain spaces.
+    If the size of k is greater than length of given string then return the given string as it is.
+
+Example 1:
+
+Input: s = "Codility We test coders", k = 14
+Output: "Codility We"
+Explaination: If we split until 14 characters then output will be "Codility We te", but since "te" is part of a word so ignore that.
+Also notice we ignore the space after "We" and "test" in our output.
+
+Example 2:
+
+Input: s = "The quick brown fox jumps over the lazy dog", k = 39
+Output: "The quick brown fox jumps over the lazy"
+
+Example 3:
+
+Input: s = "Why not", k = 39
+Output: "Why not"
+Explaination: Since the size of 'k' is greater than the length of given string so we return the string as it is.
+
+
+
+/*/
+
 
 public class Solution {
 

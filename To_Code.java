9da5c601edaa8c5258407ae5775a959df15cdaca
dheemanthp <dvp499@gmail.com>
2019-10-17 20:46:23 -0700

    Update To_Code.java

--- To_Code.java
+++ To_Code.java
@@ -1,4 +1,9 @@
 Medium: https://leetcode.com/problems/sum-of-subarray-minimums
+https://leetcode.com/discuss/interview-question/275785/Facebook-or-Phone-screen-or-Count-subsets
+good solution try : https://leetcode.com/playground/8QTQRWXA
+https://leetcode.com/discuss/interview-question/268604/Google-interview-Number-of-subsets
+
+https://www.geeksforgeeks.org/sum-of-all-subarrays/ O(n)
 Just finding subarray's is with a formula a[i] *(n-i)+ (i+1)
 
 Easy :https://leetcode.com/problems/verifying-an-alien-dictionary/

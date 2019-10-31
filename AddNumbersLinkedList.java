22fb560cc72ca4cba1ba9087acceb5653d6cde87
dheemanthp <dvp499@gmail.com>
2019-10-30 20:20:48 -0700

    Update AddNumbersLinkedList.java

--- AddNumbersLinkedList.java
+++ AddNumbersLinkedList.java
@@ -1,3 +1,5 @@
+//There are 2 variants of this problems
+//I have solved both below.
 //https://leetcode.com/problems/add-two-numbers/submissions/
 /**
  * Definition for singly-linked list.
@@ -43,4 +45,55 @@ public class Solution {
         return head;
     }
 }
+//================================================================================================
 
+https://leetcode.com/problems/add-two-numbers-ii/submissions/
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+               
+        if( l1 == null) return l2;
+        if( l2 == null) return l1;
+        int carry = 0;
+        //head is the first node
+        //prev is the running pointer
+        ListNode last = null;ListNode rest = null;
+        
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+
+
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        while(s1.size() != 0 ||s2.size() != 0 || carry == 1){
+            //if a node is null , just use 0
+            int input1 = (s1.size() != 0) ?s1.pop():0;  
+            int input2 = (s2.size() != 0) ?s2.pop():0;  
+            //always store the reminder
+            int val = (input1 + input2 + carry)%10;
+            System.out.println("the value is : " + val);
+            //always store the quotient
+            carry = (input1 +input2 + carry)/10;
+            //if head is null store the first node here
+            if(last == null) {
+                last = new ListNode(val);
+                //7 -> 8 -> 0 -> 7
+            //otherwise update the prev 
+            } else {
+                //every new node added is pointing to previously added node
+                rest = new ListNode(val);
+                rest.next = last;
+                last = rest;
+            }
+        }
+        //return head
+        return last;
+    }
+}

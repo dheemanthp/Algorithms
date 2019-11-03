486b120d896952f01b970c6bbb6370c30010eb65
dheemanthp <dvp499@gmail.com>
2019-11-02 20:46:22 -0700

    Update AddBinary.java

--- StringManipulation/AddBinary.java
+++ StringManipulation/AddBinary.java
@@ -7,14 +7,12 @@ class Solution {
         
         if(a.length() == 0) return b;
         if(b.length() == 0) return a;
-        //create char arrays
-
         int lengthA = a.length() -1;
-
         int lengthB = b.length() -1;
         //String Builder
         StringBuilder SB = new StringBuilder();
         int carry = 0;
+        //loop until one or all the conditions satisfy
         while (lengthA >= 0 || lengthB >= 0 || carry == 1) {
             int X = lengthA >=0 ? Character.getNumericValue(a.charAt(lengthA)): 0;
             int Y = lengthB >=0 ? Character.getNumericValue(b.charAt(lengthB)): 0;

2de1892603c99c59e0bdd70dab06dc72d3c68371
dheemanthp <dvp499@gmail.com>
2019-11-02 21:20:35 -0700

    Create merge-2sorted-array.java

---
+++ Arrays/merge-2sorted-array.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/merge-sorted-array/
+//O(n)
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums1.length == 0 || nums2.length == 0) return;
+        int nums1L = nums1.length-1;
+        int nums2L = n-1;
+        //found the end of first array which contains valid numbers
+        int nums1_end = m-1;
+        while (nums1_end >= 0 && nums2L >= 0) {
+            //check the last item in the first array and last item in second array
+            if(nums1[nums1_end] <= nums2[nums2L]) {
+                nums1[nums1L--] = nums2[nums2L--];
+            } else{
+                nums1[nums1L--] = nums1[nums1_end--];
+            }
+        }
+        //update first array with left over elements first array
+        while(nums1_end >= 0){
+            nums1[nums1L--] = nums1[nums1_end--];
+        }
+        //update first array with left over elements from second array
+        while(nums2L >= 0){
+            nums1[nums1L--] = nums2[nums2L--];
+        }
+        return;
+    }
+}

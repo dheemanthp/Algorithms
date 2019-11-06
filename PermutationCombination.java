358c92b8b49b7626816800d2f3981088ab5890c3
dheemanthp <dvp499@gmail.com>
2019-11-06 13:18:37 -0800

    Update PermutationCombination.java

--- PermutationCombination.java
+++ PermutationCombination.java
@@ -11,7 +11,22 @@ Output:
   [1,2],
   []
 ]
+While iterating through all numbers, for each new number, we can either pick it or not pick it
+1, if pick, just add current number to every existing subset.
+2, if not pick, just leave all existing subsets as they are.
+We just combine both into our result.
+
+For example, {1,2,3} intially we have an emtpy set as result [ [ ] ]
+Considering 1, if not use it, still [ ], if use 1, add it to [ ], so we have [1] now
+Combine them, now we have [ [ ], [1] ] as all possible subset
+
+Next considering 2, if not use it, we still have [ [ ], [1] ], if use 2, just add 2 to each previous subset, we have [2], [1,2]
+Combine them, now we have [ [ ], [1], [2], [1,2] ]
+
+Next considering 3, if not use it, we still have [ [ ], [1], [2], [1,2] ], if use 3, just add 3 to each previous subset, we have [ [3], [1,3], [2,3], [1,2,3] ]
+Combine them, now we have [ [ ], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3] ]
 */
+
 
 class Solution {
 public List<List<Integer>> subsets(int[] nums) {

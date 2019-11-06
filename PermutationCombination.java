0a97b7798888c406d8e7d0ee8ac8fc83c86180e8
dheemanthp <dvp499@gmail.com>
2019-11-06 13:16:28 -0800

    Update PermutationCombination.java

--- PermutationCombination.java
+++ PermutationCombination.java
@@ -1,3 +1,38 @@
+/*https://leetcode.com/problems/subsets/   -> Combination problem
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+class Solution {
+public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int n : nums){
+            int size = result.size();
+            for(int i=0; i<size; i++){
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(n);
+                result.add(subset);
+            }
+        }
+        return result;
+    }
+
+}
+
+
+
+
 
 public class Solution {
     

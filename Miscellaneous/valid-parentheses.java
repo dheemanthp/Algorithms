767581674682dc64bef5ac4a05ca37721b2a5bb5
dheemanthp <dvp499@gmail.com>
2019-11-09 18:25:05 -0800

    Update valid-parentheses.java

--- Miscellaneous/valid-parentheses.java
+++ Miscellaneous/valid-parentheses.java
@@ -1,4 +1,37 @@
 //https://leetcode.com/problems/valid-parentheses/
+//When you encounter open braces , Push only closed Braces
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null) return false;
+        if(s.length() == 0) return true;
+        
+        //keep pushing open braces on to the stack until you encounter an closed bracket
+        Stack<Character> C = new Stack<>();
+        char prev;
+        //char[] charArray = s.toCharArray();
+        for(int i = 0 ;i < s.length() ;i++) {
+            
+            switch(s.charAt(i)) {
+                //Push Only Closing Brackets
+                case '(' : C.push(')');break;
+                case '{' : C.push('}');break;
+                case '[' : C.push(']');break;
+                default:
+                    //Return false if there is no match
+                    if(C.isEmpty() || s.charAt(i) != C.peek()) { 
+                        return false;
+                    } else{
+                        C.pop();
+                    }
+                break;
+            }
+        }
+        if(C.size() == 0) return true;
+        return false;
+    }
+}
+
+//Alternative Solution
 class Solution {
     public boolean isValid(String s) {
         if(s == null) return false;

c842df8a323a68e10e9b0fbb8929c4cd8d6ef265
dheemanthp <dvp499@gmail.com>
2019-11-10 22:13:29 -0800

    Create firstBadVersion.java

---
+++ BinarySearch/firstBadVersion.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/first-bad-version/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        //approach is using Binary search, either go to the left or right
+        //left , if we hit a bad one
+        //right , if we hit a good one
+        
+        int mid = 0 ;
+        int left  = 0;
+        int right  = n;
+        //eventually left == right , this is the exit criteria
+        while(left < right) {
+            mid = left + (right-left)/2;
+            System.out.println("start the mid value is " + mid + " left "+ left + " right " + right);
+            if(isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+            System.out.println("end the mid value is " + mid + " left "+ left + " right " + right);
+        }
+        
+        return right;
+    }
+}

40d6fb9169263de67292b40b643d4cc37a7dc2f6
dheemanthp <dvp499@gmail.com>
2019-11-30 01:44:37 -0800

    Update SubArrayWithTargetSumNegativePositiveNumbers.java

--- DynamicProgramming/SubArrayWithTargetSumNegativePositiveNumbers.java
+++ DynamicProgramming/SubArrayWithTargetSumNegativePositiveNumbers.java
@@ -1,4 +1,35 @@
+//Related post
+/*
+https://www.programcreek.com/2014/10/leetcode-maximum-size-subarray-sum-equals-k-java/
+LeetCode – Maximum Product Subarray (Java)
+LeetCode – Maximum Subarray (Java)
+LeetCode – Subarray Sum Equals K (Java)
+Maximum Sum of Subarray Close to K
+
+*/
 import java.util.HashMap;
+
+
+https://www.programcreek.com/2014/10/leetcode-maximum-size-subarray-sum-equals-k-java/
+public int maxSubArrayLen(int[] nums, int k) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(0, -1);
+    int result = 0;
+    int sum = 0;
+ 
+    for(int i=0; i<nums.length; i++){
+        sum += nums[i];
+        if(map.containsKey(sum - k)){
+            result = Math.max(result, i - map.get(sum - k));
+        }
+        map.putIfAbsent(sum, i);
+    }
+ 
+    return result;
+}
+
+
+
 
 public class SubArrayWithTargetSumNegativePositiveNumbers{
 

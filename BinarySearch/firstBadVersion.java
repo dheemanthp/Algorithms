a8f9ff69929c2805693804c4c28825e466435294
dheemanthp <dvp499@gmail.com>
2019-12-01 21:56:26 -0800

    Update firstBadVersion.java

--- BinarySearch/firstBadVersion.java
+++ BinarySearch/firstBadVersion.java
@@ -2,6 +2,29 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+//I got it in FB mock test --- this passed
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end- start)/2;
+            //System.out.println(" the value of start " + start + " end " + end);
+            //System.out.println(" the value of mid is " + mid);
+            // go to the left
+            if(isBadVersion(mid) == true) end = mid;
+            else start = mid + 1;
+        }
+        return end;
+    }
+}
+
+
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         //approach is using Binary search, either go to the left or right

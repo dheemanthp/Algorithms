4495a6edde3a6aa784e9706ca2b94aa45a71d145
dheemanthp <dvp499@gmail.com>
2020-01-11 20:05:46 -0800

    Create duplicates_in_string_array.java

---
+++ HashMap/duplicates_in_string_array.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Leet442 {
+  
+    public static void main(String args[]) { 
+        String[] str = { "dheem","bheem","dheem", "kleem", "mleem", "chleem" , "kleem"};
+        HashMap<String,Integer> H = new HashMap<>();
+        for(int i = 0; i< str.length; i++) {
+            H.put(str[i],H.getOrDefault(str[i],0)+1);
+            
+            /*
+            Elimiate boiler plate code
+            H.getorDefault(str[i],)
+            if(H.containsKey(str[i])){
+                H.put(str[i],H.get(str[i]) + 1);
+            } else{
+                H.put(str[i],1);
+            }*/
+        }
+        
+        for(String key : H.keySet()) {
+            if(H.get(key) > 1) {
+                System.out.println(key + " is a duplicate");
+            }
+        }
+    } 
+} 

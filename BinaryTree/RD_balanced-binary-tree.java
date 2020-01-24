17253e84dd22218fad475fc99581fd02593161d9
dheemanthp <dvp499@gmail.com>
2020-01-23 17:47:49 -0800

    Update RD_balanced-binary-tree.java

--- BinaryTree/RD_balanced-binary-tree.java
+++ BinaryTree/RD_balanced-binary-tree.java
@@ -27,20 +27,30 @@ https://leetcode.com/problems/balanced-binary-tree/
 //2.The second method is based on DFS. Instead of calling depth() explicitly for each child node, we return the height of the //current node in DFS recursion. When the sub tree of the current node (inclusive) is balanced, the function dfsHeight() returns a non-negative value as the height. Otherwise -1 is returned. According to the leftHeight and rightHeight of the two children, the parent node could check if the sub tree is balanced, and decides its return value.
 //In this bottom up approach, each node in the tree only need to be accessed once. Thus the time complexity is O(N), better //than the first solution.
 
-//O(n)
+//O(n) going with Second Method
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(helper(root) != -1) return true;
+        int output = helper(root);
+        if(output != -1) {
+            System.out.println(" the output is " + output);
+            return true;
+        }
         else return false;
     }
 
     public int helper(TreeNode root) {
+        /*if(root!= null)
+        System.out.println(" the root value is " + root.val);
+        else
+        System.out.println(" the root is null ");*/
         if(root == null) return 0;
         int left = helper(root.left);
-        if(left == -1) return -1;
         int right = helper(root.right);
-        if(right == -1) return -1;
-        if(Math.abs(left-right) > 1) return -1;
-        return Math.max(left, right) + 1;
+        if(left == -1 || right == -1) return -1; //return -1
+        //System.out.println(" left is " + left);
+        //System.out.println(" right is " + right);
+        if(Math.abs(left-right) > 1) return -1; //calculate if the height delta is more than one 
+        //System.out.println(" the Math.max value is " +(Math.max(left, right) + 1 ));
+        return Math.max(left, right) + 1;//returning the height of the tree
     }
 }

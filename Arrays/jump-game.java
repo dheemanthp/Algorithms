126f5eb1f5b370e619bb7169f32a1ca58b2f2392
dheemanthp <dvp499@gmail.com>
2020-04-25 04:28:17 -0700

    Update jump-game.java

--- Arrays/jump-game.java
+++ Arrays/jump-game.java
@@ -4,61 +4,53 @@ https://leetcode.com/problems/jump-game/
 
 class Solution {
     public boolean canJump(int[] nums) {
-        return canJump(nums,nums.length);
-    }
-    boolean canJump(int A[], int n) {
-    
-    int last=n-1; // this is tracking the index value 
-    int i,j;
-    //start from last but one element
-    
-    for(i=n-2;i>=0;i--){
-        System.out.print(" the current value " + A[i] + " and index value is " + i + " total " + (A[i]+i));
-        System.out.println(" the last(index) is " + last);
-        //idea is simple 
-        //the last but one element and it's index value should be greater than or equal to the next element's index
-        //in the failure case ,
-        // the last but one element is 0 , and it's index is 3 , hence (3+0) is not greater than next element's index 4
-        // whose value is 4
+        //Approach 4: Greedy
+        /*
+        Iterating right-to-left, for each position we check if there is a potential jump that reaches a GOOD index (currPosition + nums[currPosition] >= leftmostGoodIndex). If we can reach a GOOD index, then our position is itself GOOD. Also, this new GOOD position will be the new leftmost GOOD index. Iteration continues until the beginning of the array. If first position is a GOOD index then we can reach the last index from the first position.*/
         
-        //consider current value + current index , check if it is greater than the next index
-        // if yes, then it is reachable  , then go back one index , and again compare with next index 
-        // otherwise then it is not reachable
-        // 
-        
-        if(i+A[i]>=last) {
-            last=i;// keep storing the current index , for next comparision
-            System.out.println(" the last(index) changed " + last);
+        if(nums == null || nums.length == 0) return false;
+        if(nums.length == 1) return true;
+        int len = nums.length;
+        int targetGoodIndex = len-1;
+        //Time complexity : O(n). We are doing a single pass through the nums array,
+        //hence nn steps, where nn is the length of array nums.
+        //Space complexity : O(1). We are not using any extra memory.
+        for(int i = len-2; i >= 0; i--) {
+        //System.out.print(" the current value " + nums[i] + " and index is " + i + " total " + (nums[i]+i));
+        //System.out.println(" the targetGoodIndex(index) is " + targetGoodIndex);
+
+            if(nums[i]+i >= targetGoodIndex) {
+                targetGoodIndex = i;
+                //System.out.println("goodIndex changed to " + targetGoodIndex);
+            } else {
+                //System.out.println("goodIndex remains same");
+            }
         }
-    }
-        // expectation is last(index) will be equal to zero
-        
-        if(last == 0) {
-            return true;
-        } else{
+        if(targetGoodIndex == 0) {
+            return true;    
+        } else {
             return false;
         }
-    }
-}
+     }
+}    
+    
+    /*
+    [2,0,0]
+     the current value 0 and index is 1 total 1 the targetGoodIndex(index) is 2
+    goodIndex remains same
+    the current value 2 and index is 0 total 2 the targetGoodIndex(index) is 2
+    goodIndex changed to 0
 
-/*
-
-[2,3,1,1,4]
- the current value 1 and index value is 3 total 4 the last(index) is 4
- the last(index) changed 3
- the current value 1 and index value is 2 total 3 the last(index) is 3
- the last(index) changed 2
- the current value 3 and index value is 1 total 4 the last(index) is 2
- the last(index) changed 1
- the current value 2 and index value is 0 total 2 the last(index) is 1
- the last(index) changed 0
-*/
-
-
-/*
-[3,2,1,0,5]
- the current value 0 and index value is 3 total 3 the last(index) is 4
- the current value 1 and index value is 2 total 3 the last(index) is 4
- the current value 2 and index value is 1 total 3 the last(index) is 4
- the current value 3 and index value is 0 total 3 the last(index) is 4
-*/
+    */
+    /*
+    [2,3,1,1,4]
+     the current value 1 and index is 3 total 4 the targetGoodIndex(index) is 4
+    goodIndex changed to 3
+    the current value 1 and index is 2 total 3 the targetGoodIndex(index) is 3
+    goodIndex changed to 2
+     the current value 3 and index is 1 total 4 the targetGoodIndex(index) is 2
+    goodIndex changed to 1
+    the current value 2 and index is 0 total 2 the targetGoodIndex(index) is 1
+    goodIndex changed to 0
+    
+    */

4ec865615aa4ad4d4cedb690cbdffd7485d440c8
dheemanthp <dvp499@gmail.com>
2021-06-04 21:20:41 -0700

    Create fibonocci.java

---
+++ fibonocci.java
@@ -0,0 +1,27 @@
+
+// 0, 1, 1, 2, 3, 5, 8....
+//Interative
+void fib(int input){
+  int first = 0, second = 1;
+  for(int i=0; i< input; i++){
+    System.out.println(" " + first );
+    int temp = first;
+    first = second;
+    second = temp + first;        
+  }
+}
+
+//Recursive
+void fibRecursion(int input){
+  int first = 0,second  =1;
+  
+  for(int i = 0 ; i< input; i++) {
+  System.out.print("  " + fibhelper(i));  
+  }
+}
+int fibhelper(int input){ 
+  if(input == 0 || input == 1) return 1;
+  else {
+    return fibhelper(input-1) + fibhelper(input-2);
+  }
+} 
